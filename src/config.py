e7e1208c3e954857e7e9f3f7fd3c2ea728cb392c
elouanXP <elouan.pulveric@gmail.com>
2026-02-13 18:09:10 +0100

    config_update

---
+++ src/config.py
@@ -0,0 +1,15 @@
+ZIP_PATH = '../data/raw/CMAPSSdata.zip'
+DATA_RAW = '../data/raw/train_FD001.txt'
+DATA_PROCESSED = '../data/processed'
+OUTPUTS_PLOTS = "outputs/plots/"
+OUTPUTS_MODELS = "outputs/models/"
+
+CLIP_RUL = 120
+VAR_THRESHOLD= 1e-5
+WINDOW = 5
+CORR_THRESHOLD = 0.1
+
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+TOP_FEATURES = 15
+N_SPLITS = 3
